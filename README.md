c2f19a4da073e3ab911cc53598ae9d5283868eb5
f.bakker@minfin.nl <f.bakker@minfin.nl>
2025-07-14 19:30:40 +0200

    WIP: draft

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
 # javascriptvoc
 A RDF-based vocabulary to model Javascript into RDF. Javascript logic can thus be represented, queried, generated, validated, analysed, transformed and reused as semantic objects themselves.
+
+# Abstract
+
+The Javascript Vocabulary provides a formal representation of the Javascript programming language, enabling the modeling and generation of Javascript code through an abstract syntax tree (AST) framework. It defines classes and properties to describe the various components of Javascript syntax, including functions, classes, statements, expressions, and data structures. Additionally, it incorporates SHACL shapes for validating Javascript code structures and supports algorithms for serializing Javascript code from RDF representations. This vocabulary enhances code generation and manipulation, bridging Javascript with broader semantic web technologies.
+
+# Description
+
+The Javascript Vocabulary formalizes the Javascript programming language, offering a structured representation of its syntax and semantics. It defines classes for different Javascript constructs, such as 'js:Statement' for statements, 'js:Function' for functions, and properties to capture relationships between these components. Central to this vocabulary is the class 'js:Node', which serves as a building block for all types of Javascript code segments. Each code unit can have attributes, such as 'js:operator' for operators within expressions and 'js:argument' for defining parameters. The vocabulary also includes SHACL shapes to validate the correctness of Javascript code structures, ensuring that generated code adheres to Javascript syntax rules. These shapes facilitate the creation of well-formed Javascript code that can be executed reliably. This comprehensive approach allows users to leverage RDF representations to generate Javascript code, enabling seamless integration of semantic technologies with programming tasks.
+    
+The Javascript Core Vocabulary models the Javascript language based on its Abstract Syntax Tree (AST), as described in [EStree](https://github.com/estree/estree). 
+
+# Objective
+
+To tackle the challenges of Javascript code generation, maintenance, reuse and validation, we present the Javascript Vocabulary - a transformative framework designed to support users in managing Javascript programming. This vocabulary enables users to (1) model and represent Javascript code constructs, (2) generate and validate Javascript code programmatically, and (3) ensure adherence to Javascript syntax and best practices. By leveraging the power of RDF and the flexibility of an abstract syntax tree, the Javascript Vocabulary enhances the efficiency and reliability of code generation in programming environments, fostering better coding practices and facilitating collaboration in software development projects.
+
+# Audience
+
+This document is intended for a diverse audience of software developers, data scientists, educators, and anyone involved in Javascript programming and code management. It aims to support users seeking to enhance their understanding and application of Javascript within the context of semantic technologies and code generation.
+
+# Status
+
+Unstable & unfinished. Work in progress.
+
+# Example - Hello World!
+
+Take for example the following Javascript code:
+
+```
+console.log("Hello world!")
+```
+
+This can be modeled in RDF using the Javascript Vocabulary:
+
+```
+prefix ex:  <https://example.org/>
+prefix js:  <https://www.javascript.org/model/def/>
+prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+# Root node of the AST (Program node)
+ex:program1
+  a js:Program ;
+  js:body ex:expressionStatement1 ;
+  js:fragment '''console.log("Hello world!");'''.  
+
+# ExpressionStatement
+ex:expressionStatement1
+  a js:ExpressionStatement ;
+  js:expression ex:callExpression1;
+  js:fragment '''console.log("Hello world!");'''.
+
+# CallExpression node
+ex:callExpression1
+  a js:CallExpression ;
+  js:callee    ex:memberExpression1 ;
+  js:arguments ex:arguments1 ;
+  js:fragment '''console.log("Hello world!")'''.
+
+# MemberExpression node
+ex:memberExpression1
+  a js:MemberExpression ;
+  js:object   ex:identifier_console ;
+  js:property ex:identifier_log ;
+  js:computed false;
+  js:fragment '''console.log'''.
+
+# Identifier 1
+ex:identifier_console
+  a js:Identifier ;
+  js:name '''console''' ;
+  js:fragment '''console'''.  
+
+# Identifier 2
+ex:identifier_log
+  a js:Identifier ;
+  js:name '''log''';
+  js:fragment '''log'''.
+
+# Literal
+ex:arguments1
+  a js:Literal ;
+  js:value '''Hello world!''' ;
+  js:raw '''\"hello world!\"''';
+  js:fragment '''"Hello world!"'''.  
+
+``` 
+ 
---
+++ Tools/RDF2Javascript.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  5 18:40:53 2023
+
+@author: Flores Bakker
+
+The Javascript Vocabulary.
+
+Usage: 
+
+1. Place an arbitrary RDF-based Abstract Syntax Tree snippet of Javascript (as turtle file *.ttl) in the input folder.
+2. In the command prompt, run 'python RDF2Javascript.py'
+3. Go to the output folder and grab your enriched turtle file, now including Javascript fragments.
+
+
+"""
+import os
+import pyshacl
+import rdflib 
+from rdflib import Namespace
+
+# Get the current working directory in which the RDF2Javascript.py file is located.
+current_dir = os.getcwd()
+
+# Set the path to the desired standard directory. 
+directory_path = os.path.abspath(os.path.join(current_dir, '..'))
+
+# namespace declaration
+js= Namespace("https://www.javascript.fin.rijksweb/model/def/")
+
+# Function to read a graph (as a string) from a file 
+def readGraphFromFile(file_path):
+    # Open each file in read mode
+    with open(file_path, 'r', encoding='utf-8') as file:
+            # Read the content of the file and append it to the existing string
+            file_content = file.read()
+    return file_content
+
+# Function to write a graph to a file
+def writeGraph(graph):
+    graph.serialize(destination=directory_path+"/javascriptvoc/Tools/Output/"+filename_stem+"-javascript.ttl", format="turtle")
+
+# Function to call the PyShacl engine so that a RDF model of an Javascript script can be serialized to Javascript code.
+def iteratePyShacl(javascript_generator, serializable_graph):
+        
+        # call PyShacl engine and apply the Javascript vocabulary to the serializable Javascript model
+        pyshacl.validate(
+        data_graph=serializable_graph,
+        shacl_graph=javascript_generator,
+        data_graph_format="turtle",
+        shacl_graph_format="turtle",
+        advanced=True,
+        inplace=True,
+        inference=None,
+        iterate_rules=False, #rather than setting this to true, it is better to do the iteration in the script as PyShacl seems to have some buggy behavior around iteration.
+        debug=False,
+        )
+        
+       
+        statusquery = serializable_graph.query('''
+            
+prefix js:  <https://www.javascript.fin.rijksweb/model/def/>
+prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+ASK
+WHERE 
+  {
+  ?script 
+        rdf:type js:Program;
+        js:fragment [].
+}
+        ''')   
+
+        resultquery = serializable_graph.query('''
+            
+prefix js:  <https://www.javascript.fin.rijksweb/model/def/>
+prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+SELECT ?javascriptFragment
+WHERE {
+  ?script 
+         rdf:type js:Program;
+         js:fragment ?javascriptFragment.
+  
+}
+
+        ''')   
+
+        # Check whether another iteration is needed. If every OWL and RDFS construct contains a javascript:syntax statement, the processing is considered done.
+        for status in statusquery:
+            print ('status = ', status)
+            if status == False:
+                writeGraph(serializable_graph)
+                iteratePyShacl(javascript_generator, serializable_graph)
+            else: 
+                 print ("File " + filename_stem+"-javascript.ttl" + " created in output folder.")
+                 writeGraph(serializable_graph)
+        
+                 for result in resultquery:
+                    javascript_fragment = result["javascriptFragment"]
+                    output_file_path = directory_path+"/javascriptvoc/Tools/Output/"+filename_stem+".py"
+                    
+                    # Write the javascript content to the output file
+                    with open(output_file_path, "w", encoding="utf-8") as file:
+                        file.write(javascript_fragment)
+                    print ("File " + filename_stem+".rq" + " created in output folder.")
+
+                 
+# loop through any turtle files in the input directory
+for filename in os.listdir(directory_path+"/javascriptvoc/Tools/Input"):
+    if filename.endswith(".ttl"):
+        file_path = os.path.join(directory_path+"/javascriptvoc/Tools/Input", filename)
+        
+        # Establish the stem of the file name for reuse in newly created files
+        filename_stem = os.path.splitext(filename)[0]
+        
+        # Get the Javascript vocabulary and place it in a string
+        javascript_generator = readGraphFromFile(directory_path+"/javascriptvoc/Specification/javascriptvoc - core.ttl")
+        
+        # Get some Javascript code represented in triples
+        javascript_script_string = readGraphFromFile(file_path) + '\n' + '\n' +  javascript_generator
+        
+        # Create a graph
+        serializable_graph = rdflib.Graph().parse(data=javascript_script_string , format="ttl")
+        serializable_graph.bind("js", js)
+                        
+        # Inform user
+        print ('\nCreating javascript fragments for file',filename, '...')
+        
+        # Call the shacl engine with the Javascript vocabulary
+        iteratePyShacl(javascript_generator, serializable_graph)
+        
+        # Inform user
+        print ('Done.')
